e90b22dccdd53ab2280ebb1cee0681d8ee6f672e
Kevin <aoxianglele2010@gmail.com>
2017-05-12 09:55:11 +0800

    Added readme.

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # dolores-server
-dolores 服务端
+dolores 服务端 提供RestFull API
+
+## 接口列表
+
+### /organization
+###### 参数: 无
+###### 返回值:
+``` json
+{
+  "departments": [ // 返回当前登录用户所有可见的部门， 父部门总是早于子部门返回
+    {
+      "description": "This is test unit's description",
+      "id": "b4a1bldhfpcl3m5lhohg",
+      "name": "Test",
+    },
+    {
+      "description": "This is test unit's description", // 部门的描述备注信息
+      "id": "b49kdrg6h302hrpggg8g", // 部门ID
+      "name": "Test", // 部门名称
+      "pid": "b4a1bldhfpcl3m5lhohg" // 父部门ID
+    },
+    {
+      "description": "This is test unit's description",
+      "id": "b4a1cqlhfpcl6964kqtg",
+      "name": "Test",
+      "pid": "b49kdrg6h302hrpggg8g"
+    }
+  ],
+  "members": [ // 返回当前登录用户的所有可见员工
+    {
+      "departmentIDs": [ // 员工所在部门id 一个员工可能在多个部门
+        "b49kdrg6h302hrpggg8g"
+      ],
+      "easemobAccount": "", // 环信ID
+      "easemobPassword": "", // 环信密码
+      "email": [ // 用户邮箱，可能有多个邮箱
+        "aoxianglele@icloud.com"
+      ],
+      "id": "b49kehg6h302jg98oi70", // 用户ID
+      "name": "Kevin.Gong", // 显示用的名字
+      "realName": "巩祥", // 真名
+      "title": [ // 职位，可能有多个
+        "Developer"
+      ],
+      "gender": 0, // 用户性别 0: 女 1: 男 其他: 未知
+      "avatarURL": "http://www.dolores.store/static/avatar/b49kehg6h302jg98oi70.png" // 用户头像url
+    }
+  ],
+  "version": 1
+}
+```
--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+
+	"github.com/DoloresTeam/organization"
+	"github.com/gin-gonic/gin"
+)
 
 // Department ...
 type Department struct {
@@ -20,21 +25,34 @@ type Person struct {
 
 func main() {
 
-	d1 := &Department{`1`, `D1`, ``}
-	d2 := &Department{`2`, `D2`, ``}
-	d3 := &Department{`3`, `D3`, `1`}
-	d4 := &Department{`4`, `D4`, `1`}
-
-	p1 := &Person{`1`, `m1`, `M1`, `developer`, []string{`1`}}
-	p2 := &Person{`2`, `m2`, `M2`, `developer`, []string{`2`}}
-	p3 := &Person{`3`, `m3`, `M3`, `developer`, []string{`1`, `3`}}
+	// d1 := &Department{`1`, `D1`, ``}
+	// d2 := &Department{`2`, `D2`, ``}
+	// d3 := &Department{`3`, `D3`, `1`}
+	// d4 := &Department{`4`, `D4`, `1`}
+	//
+	// p1 := &Person{`1`, `m1`, `M1`, `developer`, []string{`1`}}
+	// p2 := &Person{`2`, `m2`, `M2`, `developer`, []string{`2`}}
+	// p3 := &Person{`3`, `m3`, `M3`, `developer`, []string{`1`, `3`}}
+	org, err := organization.NewOrganizationWithSimpleBind(`dc=dolores,dc=store`, `localhost`, `cn=admin,dc=dolores,dc=store`, `secret`, 389)
+	if err != nil {
+		panic(err)
+	}
+	departments, err := org.OrganizationUnitByMemberID(`b49kehg6h302jg98oi70`)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	members, err := org.OrganizationMemberByMemberID(`b49kehg6h302jg98oi70`)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	router := gin.Default()
 
 	router.GET(`/organization`, func(c *gin.Context) {
 		c.JSON(200, map[string]interface{}{
-			`departments`: []*Department{d1, d3, d4, d2},
-			`members`:     []*Person{p1, p2, p3},
+			`departments`: departments,
+			`members`:     members,
 			`version`:     1,
 		})
 	})
