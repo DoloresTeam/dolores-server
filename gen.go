39a02dafa439482d60db623cff9367ef51ef22ff
Kevin <aoxianglele2010@gmail.com>
2017-06-04 01:10:34 +0800

    Add gen random password helper.

---
+++ gen.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"crypto/rand"
+	"io"
+)
+
+var stdChars = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+,.?/:;{}[]`~")
+
+func newPassword() string {
+	return rand_char(8, stdChars)
+}
+
+func rand_char(length int, chars []byte) string {
+	new_pword := make([]byte, length)
+	random_data := make([]byte, length+(length/4)) // storage for random bytes.
+	clen := byte(len(chars))
+	maxrb := byte(256 - (256 % len(chars)))
+	i := 0
+	for {
+		if _, err := io.ReadFull(rand.Reader, random_data); err != nil {
+			panic(err)
+		}
+		for _, c := range random_data {
+			if c >= maxrb {
+				continue
+			}
+			new_pword[i] = chars[c%clen]
+			i++
+			if i == length {
+				return string(new_pword)
+			}
+		}
+	}
+}
